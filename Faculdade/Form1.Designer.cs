8c56fc65a1385c007bd5f2258fe66cc4acd319a9
DevPio <luccaspio890@gmail.com>
2021-11-03 02:12:36 -0300

    Adicionar arquivos de projeto.

---
+++ Faculdade/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Faculdade
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nome = new System.Windows.Forms.Label();
+            this.NomeTXT = new System.Windows.Forms.TextBox();
+            this.CelularTXT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailTXT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Adicionar = new System.Windows.Forms.Button();
+            this.PasswordTXT = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(46, 74);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(50, 20);
+            this.Nome.TabIndex = 0;
+            this.Nome.Text = "Nome";
+            // 
+            // NomeTXT
+            // 
+            this.NomeTXT.Location = new System.Drawing.Point(46, 118);
+            this.NomeTXT.Name = "NomeTXT";
+            this.NomeTXT.Size = new System.Drawing.Size(357, 27);
+            this.NomeTXT.TabIndex = 1;
+            // 
+            // CelularTXT
+            // 
+            this.CelularTXT.Location = new System.Drawing.Point(46, 234);
+            this.CelularTXT.Name = "CelularTXT";
+            this.CelularTXT.Size = new System.Drawing.Size(357, 27);
+            this.CelularTXT.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Celular";
+            // 
+            // EmailTXT
+            // 
+            this.EmailTXT.Location = new System.Drawing.Point(46, 350);
+            this.EmailTXT.Name = "EmailTXT";
+            this.EmailTXT.Size = new System.Drawing.Size(357, 27);
+            this.EmailTXT.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // Adicionar
+            // 
+            this.Adicionar.Location = new System.Drawing.Point(241, 397);
+            this.Adicionar.Name = "Adicionar";
+            this.Adicionar.Size = new System.Drawing.Size(342, 29);
+            this.Adicionar.TabIndex = 6;
+            this.Adicionar.Text = "Adicionar";
+            this.Adicionar.UseVisualStyleBackColor = true;
+            this.Adicionar.Click += new System.EventHandler(this.Adicionar_Click);
+            // 
+            // PasswordTXT
+            // 
+            this.PasswordTXT.Location = new System.Drawing.Point(431, 118);
+            this.PasswordTXT.Name = "PasswordTXT";
+            this.PasswordTXT.Size = new System.Drawing.Size(357, 27);
+            this.PasswordTXT.TabIndex = 8;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(431, 74);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(70, 20);
+            this.Password.TabIndex = 7;
+            this.Password.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PasswordTXT);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Adicionar);
+            this.Controls.Add(this.EmailTXT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CelularTXT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NomeTXT);
+            this.Controls.Add(this.Nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox NomeTXT;
+        private System.Windows.Forms.TextBox CelularTXT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmailTXT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Adicionar;
+        private System.Windows.Forms.TextBox PasswordTXT;
+        private System.Windows.Forms.Label Password;
+    }
+}
+
---
+++ Faculdade/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace Faculdade
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection conection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string stringConectio = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=User;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+             this.conection = new SqlConnection(stringConectio);
+
+
+            var comandoAll = this.conection.CreateCommand();
+
+
+
+           comandoAll.CommandText = "select * from Users";
+
+            this.conection.Open();
+
+            var result = comandoAll.ExecuteReader();
+
+            while (result.Read())
+            {
+              var r = result;
+            }
+
+            this.conection.Close();
+        }
+
+        private void Adicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var comando = this.conection.CreateCommand();
+
+
+                comando.CommandText = "INSERT INTO Users(name,email,password)";
+
+                comando.CommandText += $"VALUES ('{NomeTXT.Text}','{EmailTXT.Text}','{PasswordTXT.Text}')";
+
+                this.conection.Open();
+
+                comando.ExecuteNonQuery();
+
+                this.conection.Close();
+            }
+            catch (Exception ex)
+            {
+
+               
+            }
+        }
+    }
+}
